cf33325fee9d99c94cdd8356bcefac5242042807
Romain Quilliot <romain.addweb@gmail.com>
2015-04-01 00:50:15 +0200

    Ajout du model Users

---
+++ src/Fubbiz/Model/Users.php
@@ -0,0 +1,74 @@
+<?php
+namespace Fubbiz\Model;
+
+
+class Users
+{
+    /**
+     * Constructeur
+     * @param object $db
+     *
+     */
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    /**
+     * Recupere et liste les utilisateurs
+     * @return array
+     *
+     */
+    public function listing()
+    {
+        $sqlQuery = "SELECT oeuvre_id, username FROM datas WHERE date = ? AND hour = ?";
+
+        return $this->db->fetchAll($sqlQuery, [date('d-m-Y'), date('H')]);
+    }
+
+
+    /**
+     * Recupere l'id de l'utilisateur
+     * @param string $username
+     * @return array
+     *
+     */
+    public function getOeuvreByUsername($username)
+    {
+        $sqlQuery = "SELECT oeuvre_id FROM datas WHERE username = ? AND date = ? AND hour = ?";
+
+        return $this->db->fetchAll($sqlQuery, [$username, date('d-m-Y'), date('H')]);
+    }
+
+
+    /**
+     * Recupere l'id de l'utilisateur
+     * @param string $username
+     * @return array
+     *
+     */
+    public function getAllStatsByUsername($username)
+    {
+        $toReturn = [];
+        $sqlQuery = "SELECT oeuvre_id, date, hour FROM datas WHERE username = ?";
+        $sqlExecuted = $this->db->fetchAll($sqlQuery, [$username]);
+
+        if (count($sqlExecuted) <= 0) {
+            return false;
+        }
+
+        $toReturn['oeuvre_id'] = $sqlExecuted[0]['oeuvre_id'];
+
+        foreach ($sqlExecuted as $date) {
+            $sqlQueryDate = "SELECT facebook, twitter, votes  FROM datas WHERE username = ? AND date = ? AND hour = ?";
+            $sqlExecutedDate = $this->db->fetchAll($sqlQueryDate, [$username, $date['date'], $date['hour']]);
+
+            foreach ($sqlExecutedDate as $data) {
+                $hour = $date['hour'];
+                $toReturn['stats'][$date['date']][$hour] = $data;
+            }
+        }
+
+        return $toReturn;
+    }
+}
